c2380e33d7d04144b220b3a012a02b10b50d9481
“Emmett <emmett.wechsler11@gmail.com>
2023-02-08 12:12:31 -0500

    more navbar

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -5,13 +5,17 @@ import logo from '../../assets/images/navbar_logo.png';
 import location from '../../assets/images/navbar_location_marker.png';
 import NavBarLinks from './NavBarLinks';
 import SearchBar from '../SearchBar';
+import flag from '../../assets/images/usa_flag.png'
+import dropDown from '../../assets/images/dropdown_arrow.png'
 
 const NavBar = () => {
     return (
         <div className="navbar-wrapper">
             <div className="navbar-top">
-                <img id="navbar-logo" src={logo}></img>
-                <div className="navbar-location-display">
+                <div className="navbar-element-border">
+                    <img id="navbar-logo" src={logo}></img>
+                </div>
+                <div className="navbar-element-border" id="navbar-location-display">
                     <img id="navbar-location-icon" src={location}></img>
                     <div id="navbar-location-textbox-wrapper">
                         <p id="navbar-hello">Hello</p>
@@ -19,6 +23,11 @@ const NavBar = () => {
                     </div>
                 </div>
                 <SearchBar />
+                <div id="navbar-language" className="navbar-element-border">
+                    <img id="flag" src={flag}/>
+                    <span>EN</span>
+                    <img id="arrow" src={dropDown} />
+                </div>
             </div>
             <NavBarLinks />
         </div>
