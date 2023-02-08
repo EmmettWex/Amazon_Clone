6212859cb25de29d8595dfe3f6611bab7d4b2ac3
“Emmett <emmett.wechsler11@gmail.com>
2023-02-08 12:30:57 -0500

    signin/out button

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './NavBar.css';
 import logo from '../../assets/images/navbar_logo.png';
 import location from '../../assets/images/navbar_location_marker.png';
@@ -8,7 +9,11 @@ import SearchBar from '../SearchBar';
 import flag from '../../assets/images/usa_flag.png'
 import dropDown from '../../assets/images/dropdown_arrow.png'
 
+
 const NavBar = () => {
+    const sessionUser = useSelector(state => state.session.user);
+    const signInOutButton = sessionUser ? 'Sign out' : 'Sign in' ;
+
     return (
         <div className="navbar-wrapper">
             <div className="navbar-top">
@@ -23,11 +28,21 @@ const NavBar = () => {
                     </div>
                 </div>
                 <SearchBar />
+                {/*
+                    potentially more functionality in navbar language
+                    should open a modal that says we only have english
+                */}
                 <div id="navbar-language" className="navbar-element-border">
                     <img id="flag" src={flag}/>
                     <span>EN</span>
                     <img id="arrow" src={dropDown} />
                 </div>
+                {/* this is the end of the section */}
+
+                <button className="navbar-login-logout">
+                    {signInOutButton}
+                </button>
+
             </div>
             <NavBarLinks />
         </div>
